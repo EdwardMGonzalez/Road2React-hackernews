c727a0b607e429d8f723c615fb75b620d8d1f623
Ed Gonzalez <Edward.M.Gonzalez@gmail.com>
2019-12-15 05:21:08 -0800

    small formatting change

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 
 class App extends Component {
     render() {
-      return (
-        <div className="App">
-         <h2>Welcome to the Road to learn React</h2>
-        </div>
-      );
+        return (
+            <div className="App">
+             <h2>Welcome to the Road to learn React</h2>
+            </div>
+        );
     }
 }
 
