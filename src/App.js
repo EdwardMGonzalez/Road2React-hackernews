7063effd8b384b4c1861624b9ab3157b01c76fdd
Ed Gonzalez <Edward.M.Gonzalez@gmail.com>
2019-12-15 18:15:55 -0800

    ES6 Arrow Functions, updated syntax

--- src/App.js
+++ src/App.js
@@ -26,18 +26,16 @@ class App extends Component {
 		const helloWorld = 'Hey, Welcome to the Road to learn React';
 		return (
 			<div className="App">
-			{list.map(function(item) {
-				return(
-					<div key={item.objectId}>
-					 	<span>
-							<a href={item.url}>{item.title}</a>
-						</span>
-						<span>{item.author}</span>
-						<span>{item.num_comments}</span>
-						<span>{item.points}</span>
-					</div>
-				);
-			})}
+			{list.map( item =>
+				<div key={item.objectId}>
+				 	<span>
+						<a href={item.url}>{item.title}</a>
+					</span>
+					<span>{item.author}</span>
+					<span>{item.num_comments}</span>
+					<span>{item.points}</span>
+				</div>
+			)}
 			</div>
 		);
 	}
