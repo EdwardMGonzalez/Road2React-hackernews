162dc9702e626d4a9ba64d463ec9aac53f83ab70
Ed Gonzalez <Edward.M.Gonzalez@gmail.com>
2019-12-19 06:02:38 -0800

    Updated failing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders author', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Jordan Walke/i);
   expect(linkElement).toBeInTheDocument();
 });
