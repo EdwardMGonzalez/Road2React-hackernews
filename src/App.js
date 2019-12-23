8f4b7d09466eca2134f2797e0e810675e44bc149
Ed Gonzalez <Edward.M.Gonzalez@gmail.com>
2019-12-23 09:23:05 -0800

    Added snapshot test and simple render test.

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ class App extends Component {
 
     return (
       <div className="page">
+        <h1>Keyword Search</h1>
         <div className="interactions">
           <Search
             value={searchTerm}
